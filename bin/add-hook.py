f7a7db23fc733c60487cd4d199a641e050049c2f
Jon Moses <jon@burningbush.us>
2024-07-30 13:18:08 -0400

    feat: Run the repo-local pre-commit hooks, if configured
    Fixes #1

--- bin/add-hook.py
+++ bin/add-hook.py
@@ -8,6 +8,29 @@ import stat
 import re
 import shutil
 
+TRAILER = """
+# Capture the previous calls result
+code=$?
+
+if [ $code -ne 0 ] ; then
+  # If it errors, just exit
+  exit $code
+elif [ -f .pre-commit-config.yaml ] ; then
+  # If the repo has it's own config, run it
+  echo "Running repo local pre-commit hooks"
+  ARGS[1]=--config="$(pwd)/.pre-commit-config.yaml"
+
+  if [ -x "$INSTALL_PYTHON" ]; then
+      exec "$INSTALL_PYTHON" -mpre_commit "${ARGS[@]}"
+  elif command -v pre-commit > /dev/null; then
+      exec pre-commit "${ARGS[@]}"
+  else
+    echo 'Unable to run local pre-commit'
+    exit 1
+  fi
+fi
+"""
+
 # Set the config path. If relative, it's relative to the repo clone root. Absolute paths also supported.
 CONFIG_PATH = os.environ.get("PRECOMMIT_CONFIG_PATH", ".pre-commit-config.yaml")
 if not os.path.isabs(CONFIG_PATH):
@@ -25,4 +48,12 @@ subprocess.run(
     ["pre-commit", "init-templatedir", target.name, "-t", hook_name, "-c", CONFIG_PATH]
 ).check_returncode()
 
-shutil.copy(os.path.join(target.name, "hooks", hook_name), hook_path)
+with open(os.path.join(target.name, "hooks", hook_name)) as src:
+    contents = src.read().replace("exec", "") + "\n" + TRAILER
+
+    with open(hook_path, "w") as dest:
+        dest.write(contents)
+
+os.chmod(
+    hook_path, os.stat(hook_path).st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH
+)
